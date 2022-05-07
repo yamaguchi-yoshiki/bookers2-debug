df20cdc727fe4a91b1beed94598e94f1a385ca1b
yamaguchi-yoshiki <102662423+yamaguchi-yoshiki@users.noreply.github.com>
2022-05-07 17:13:26 +0000

    [Add] 応用課題8b 5/7

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'jquery-rails'
 gem 'impressionist'
+gem 'chart-js-rails', '~> 0.1.4'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,21 +6,39 @@ class UsersController < ApplicationController
     @books = @user.books
     @book = Book.new
 
-    # 投稿数
+    # 日数別投稿数
+    @today = Time.zone.now.strftime("%-m月 %-d日")
     @today_book = @books.created_today
+    @yesterday = 1.day.ago.strftime("%-m月 %-d日")
     @yesterday_book = @books.created_yesterday
+    @two_days_ago = 2.day.ago.strftime("%-m月 %-d日")
+    @two_days_ago_book = @books.created_2days_ago
+    @three_days_ago = 3.day.ago.strftime("%-m月 %-d日")
+    @three_days_ago_book = @books.created_3days_ago
+    @four_days_ago = 4.day.ago.strftime("%-m月 %-d日")
+    @four_days_ago_book = @books.created_4days_ago
+    @five_days_ago = 5.day.ago.strftime("%-m月 %-d日")
+    @five_days_ago_book = @books.created_5days_ago
+    @six_days_ago = 6.day.ago.strftime("%-m月 %-d日")
+    @six_days_ago_book = @books.created_6days_ago
+    # 前日比
     if @yesterday_book.count == 0
       @the_day_before = false
     else
       @the_day_before = @today_book.count / @yesterday_book.count.to_f * 100
     end
+    # 週別投稿数
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
+    # 先週比
     if @last_week_book.count == 0
       @the_week_before = false
     else
       @the_week_before = @this_week_book.count / @last_week_book.count.to_f * 100
     end
+    # グラフ
+    @chartlabels = [@six_days_ago, @five_days_ago, @four_days_ago, @three_days_ago, @two_days_ago, @yesterday, @today]
+    @chartdatas = @this_week_book
 
     # DM機能
     @current_user_entry = Entry.where(user_id: current_user.id)
